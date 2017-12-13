35f99615157f9d205af73a88932e308e6687eeec
Keian Barton <Keian.Barton@Civica.co.uk>
2017-12-13 16:56:09 +0000

    Adding data annotations and CRUD methods

---
+++ ForgingAhead/Controllers/CharacterController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ForgingAhead.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ForgingAhead.Controllers
+{
+    public class CharacterController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CharacterController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Create(Character character)
+        {
+            _context.Characters.Add(character);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Index()
+        {
+            var model = _context.Characters.ToList();
+            return View(model);
+        }
+
+        public IActionResult GetActive()
+        {
+            var model = _context.Characters.Where(c => c.IsActive).ToList();
+            return View(model);
+        }
+
+        public IActionResult Details(string name)
+        {
+            var model = _context.Characters.FirstOrDefault(c => c.Name == name);
+            return View(model);
+        }
+
+        public IActionResult Update(Character character)
+        {
+            _context.Entry(character).State = EntityState.Modified;
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(string name)
+        {
+            var original = _context.Characters.FirstOrDefault(c => c.Name == name);
+            if(original != null)
+            {
+                _context.Characters.Remove(original);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ForgingAhead/Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ForgingAhead.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // Singleton
+        public static readonly ApplicationDbContext _instance = new ApplicationDbContext();
+        public static ApplicationDbContext Instance { get { return _instance; } }
+        private ApplicationDbContext() { }
+
+        // Data
+        public DbSet<Character> Characters { get; set; }
+        public DbSet<Equipment> Equipment { get; set; }
+    }
+}
