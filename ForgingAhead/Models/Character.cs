ba104fc5ebf50ea8b6a74321a2bb305b463fb47a
Keian Barton <Keian.Barton@Civica.co.uk>
2017-12-20 09:16:14 +0000

    Added back-end validation

--- ForgingAhead/Models/Character.cs
+++ ForgingAhead/Models/Character.cs
@@ -6,16 +6,34 @@ namespace ForgingAhead.Models
 {
     public class Character
     {
-        [HiddenInput]
         [Key]
+        [HiddenInput]
+        [Range(1, int.MaxValue)]
+        [Required]
         public int Id { get; private set; }
 
+        [MinLength(3)]
+        [Required]
         public string Name { get; set; }
+
         [Display(Name = "Is Active")]
+        [Required]
         public bool IsActive { get; set; }
+
+        [Range(1, 99)]
+        [Required]
         public int Level { get; set; }
+
+        [Range(1, 99)]
+        [Required]
         public int Strength { get; set; }
+
+        [Range(1, 99)]
+        [Required]
         public int Dexterity { get; set; }
+
+        [Range(1, 99)]
+        [Required]
         public int Intelligence { get; set; }
 
         public List<Equipment> Equipment { get; set; }
--- ForgingAhead/Models/Equipment.cs
+++ ForgingAhead/Models/Equipment.cs
@@ -5,12 +5,22 @@ namespace ForgingAhead.Models
 {
     public class Equipment
     {
-        [HiddenInput]
         [Key]
+        [HiddenInput]
+        [Range(1, int.MaxValue)]
+        [Required]
         public int Id { get; private set; }
 
+        [MinLength(3)]
+        [Required]
         public string Name { get; set; }
+
+        [Range(1, 99)]
+        [Required]
         public int Attack { get; set; }
+
+        [Range(1, 99)]
+        [Required]
         public int Defense { get; set; }
     }
 }
